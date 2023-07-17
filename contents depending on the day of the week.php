16f576142e66c83e1c97af1652d7c13931fc9c6d
EnotPotoskun15 <109222230+EnotPotoskun15@users.noreply.github.com>
2023-07-17 10:09:21 +0300

    Update visual "contents depending on the day of the week".php

--- contents depending on the day of the week.php
+++ contents depending on the day of the week.php
@@ -1,10 +1,11 @@
 <div style="font-size:15px; display:inline;">
 <?php
 $n = date("w", mktime(0,0,0,date("m"),date("d"),date("Y")));
+
 if (($n == '0') || ($n == '6')) {
-echo "9:00 - 23:00";
+  echo "9:00 - 23:00";
 }else{
-echo "8:30 - 23:00";
+  echo "8:30 - 23:00";
 }
 ?>
 </div>
